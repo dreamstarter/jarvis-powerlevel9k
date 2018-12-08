d86046c090cb2e0a391726e194812d9152835e40
sankios <mattia.sanchioni.dev@gmail.com>
2018-12-08 23:36:17 +0100

    impostata linea, prime modifiche fatte

---
+++ alias.zsh
@@ -0,0 +1,26 @@
+# ALIAS
+
+# configurazioni
+alias hostsSet="sudo vim /private/etc/hosts"	# modifica gli host bloccati
+
+# apertura programmi
+alias atom="open -a Atom"
+alias intellij="open -a IntelliJ\ IDEA\ CE"
+alias pycharm="open -a PyCharm\ CE"
+alias matlab="open -a MATLAB_R2018b"
+
+# gestione zsh
+alias zshconfig="open ~/.zshrc"
+alias zshreload="source ~/.zshrc"
+alias zshatom="open -a Atom ~/.zshrc"
+
+# cd --> non funziona
+alias cd+="cd && ls"
+alias cd+a="cd && ls -a"
+
+# cartelle predefinite
+alias università="cd /Users/lisa/Library/Mobile\ Documents/3L68KQB4HG~com~readdle~CommonDocuments/Documents/Università"
+alias notability="cd /Users/lisa/Google\ Drive/Notability\ Mattia"source /usr/local/opt/powerlevel9k@0.6.3/powerlevel9k.zsh-theme
+
+# powerlevel9k
+alias get_all_colors='for code ({000..255}) print -P -- "$code: %F{$code}This is how your text would look like%f"'
---
+++ jarvis.zsh
@@ -0,0 +1,77 @@
+
+
+POWERLEVEL9K_STATUS_VERBOSE=false
+POWERLEVEL9K_PROMPT_ADD_NEWLINE=true
+
+POWERLEVEL9K_LEFT_SEGMENT_SEPARATOR=''    #'\uE0B0'
+POWERLEVEL9K_LEFT_SUBSEGMENT_SEPARATOR=''   #'\uE0B1'
+POWERLEVEL9K_RIGHT_SEGMENT_SEPARATOR=''
+POWERLEVEL9K_RIGHT_SUBSEGMENT_SEPARATOR=''
+
+# ICONS
+# custom icons
+# echo -n prints the line without starting a newline at the end of it
+# ‘\ue781’ is the icon you want to display — it needs to be in quotes and start with \u to tell the shell to interpret it as a unicode escape sequence
+# JavaScript is the text you want to be shown following the icon
+# POWERLEVEL9K_CUSTOM_JAVASCRIPT="echo -n '\ue781' JavaScript"
+# POWERLEVEL9K_CUSTOM_JAVASCRIPT_FOREGROUND="black"
+# POWERLEVEL9K_CUSTOM_JAVASCRIPT_BACKGROUND="yellow"
+POWERLEVEL9K_CUSTOM_SEPARATOR="echo -n '\u2570''\U2500'"
+POWERLEVEL9K_CUSTOM_SEPARATOR_BACKGROUND="black"
+POWERLEVEL9K_CUSTOM_SEPARATOR_FOREGROUND="white"
+
+# # User configuration
+# POWERLEVEL9K_MODE='nerdfont-complete'
+# POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(icons_test)
+# POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
+#     context
+#     dir_writable
+#     newline
+#     custom_separator
+#     dir
+#     # vcs
+# )
+#
+# POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
+#     battery
+#     time
+# )
+# plugins=(
+#   git
+#   osx
+#   python
+#   battery
+# )
+
+# CONTEXT
+POWERLEVEL9K_CONTEXT_TEMPLATE="Jarvis \ue711"
+POWERLEVEL9K_CONTEXT_DEFAULT_BACKGROUND="014"
+POWERLEVEL9K_CONTEXT_DEFAULT_FOREGROUND="000"
+
+# DIR_WRITABLE
+POWERLEVEL9K_DIR_WRITABLE_FORBIDDEN_BACKGROUND="001"
+POWERLEVEL9K_DIR_WRITABLE_FORBIDDEN_FOREGROUND="000"
+
+# DIR
+POWERLEVEL9K_DIR_HOME_BACKGROUND="021"
+POWERLEVEL9K_DIR_HOME_FOREGROUND="white"
+POWERLEVEL9K_DIR_HOME_SUBFOLDER_BACKGROUND="027"
+POWERLEVEL9K_DIR_HOME_SUBFOLDER_FOREGROUND="white"
+POWERLEVEL9K_DIR_DEFAULT_BACKGROUND="033"
+POWERLEVEL9K_DIR_DEFAULT_FOREGROUND="white"
+
+# BATTERY
+# POWERLEVEL9K_BATTERY_LEVEL_FOREGROUND=(darkred orange4 yellow4 yellow4 chartreuse3 green3 green4 darkgreen)
+POWERLEVEL9K_BATTERY_STAGES=(
+    '\uf579' '\uf57a' '\uf57b' '\uf57c' 'uf57d' '\uf57e' '\uf57f' '\uf580' '\uf581' '\uf578'
+)
+POWERLEVEL9K_BATTERY_CHARGED_FOREGROUND="021"
+POWERLEVEL9K_BATTERY_CHARGING_FOREGROUND="014"                  #Color to indicate a charging battery.
+POWERLEVEL9K_BATTERY_LOW_THRESHOLD=10	               #Threshold to consider battery level critical.
+POWERLEVEL9K_BATTERY_LOW_FOREGROUND="160"	               #Color to indicate critically low charge level.
+POWERLEVEL9K_BATTERY_DISCONNECTED_FOREGROUND="033"
+POWERLEVEL9K_BATTERY_VERBOSE= true	                   #Display time remaining next to battery level.
+
+# TIME
+POWERLEVEL9K_TIME_FOREGROUND='white'
+POWERLEVEL9K_TIME_BACKGROUND='black'
